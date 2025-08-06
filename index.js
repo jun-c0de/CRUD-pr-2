29a3f9226c6ac43a2aa893a7faa2195f2b20a017
jun-c0de <sh48520414@gmail.com>
2025-08-06 09:05:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const bookRoutes = require("./routes/books")
 
-const userRouter =require("./routes/user")
 
 app.use(express.json())
-app.use("/users",userRouter)
+app.use("/books",bookRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello Express!")
