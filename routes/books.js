2a00088c4aa727f8710f949c5ad59a4317e69cea
jun-c0de <sh48520414@gmail.com>
2025-08-06 09:17:35 +0900

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -8,56 +8,140 @@ let books = [
     { id: 3, title: "css", auther: "김**" },
 ];
 
-let initId = 4
-const findIndexId = (idParams) => { 
-    return books.findIndex(p => p.id == Number(idParams)) 
+
+const findeIndexId = (idParam) => {
+    return books.findIndex(p => p.id == Number(idParam))
 }
 
 router.post('/', (req, res) => {
     try {
         const { title, auther } = req.body
 
-        if (typeof title !== 'string' || auther.trim() == '' ||
-            typeof auther !== 'string' || auther.trim() == '') {
-            return res.status(400).json({ message: "비워있지 않은 문자열 이어야 합니다" })
+        if (typeof title !== "string" || title.trim() === "") {
+            return res.status(400).json({ message: "title은 비어있지 않은 문자열이어야 합니다." });
+        }
+        if (typeof auther !== "string" || auther.trim() === "") {
+            return res.status(400).json({ message: "auther는 비어있지 않은 문자열이어야 합니다." });
         }
 
-        const newBooks = {
-            id: initId++,
+        const nextId = books.length ? Math.max(...books.map(b => b.id)) + 1 : 1
+
+        const newBook = {
+            id: nextId,
             title: title,
             auther: auther
         }
-        books.push(newBooks)
-        res.status(200).json({ message: "책 등록 완료", books })
+        books.push(newBook)
+        res.status(200).json({ message: "도서  추가 하기", books })
     } catch (error) {
-        console.error("책 등록중 오류", error)
+        console.error(" 도서 추가 하기 중 오류")
         res.status(500).json({ message: "서버오류" })
     }
 })
-
-router.get('/books', (req, res) => {
+router.get('/', (req, res) => {
     try {
-        res.status(201).json({ message: "사용자 조회 완료", books })
+        res.status(200).json({ message: "전체도서 가져오기", books })
     } catch (error) {
-        console.error("게시글 불러오기 중 오류", error)
+        console.error("전체 도서 가져오기 중 오류")
         res.status(500).json({ message: "서버오류" })
     }
 })
-
-router.get('/books/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     try {
         const bookId = Number(req.params.id)
-        const index = findIndexId(bookId)
-        console.log(index)
-        if(index === -1){
-            return res.status(404).json({message:"책 없음"})
+
+        const index = findeIndexId(bookId)
+        if (index === -1) {
+            return res.status(404).json({ message: "유효하지 않은 id 값입니다." });
         }
-        res.status(200).json({ message: "책 조회 완료", book: books[index] })
+        console.log(index)
+        res.status(200).json({ message: "도서 한권 가져오기", book: books[index] })
     } catch (error) {
-        console.error("책 불러오기 중 오류", error)
+        console.error("도서 1권 가져오기 중 오류")
+        res.status(500).json({ message: "서버오류" })
+    }
+})
+router.put('/:id', (req, res) => {
+    try {
+        const bookId = Number(req.params.id)
+
+        const index = findeIndexId(bookId)
+        if (index === -1) {
+            return res.status(404).json({ message: "유효하지 않은 id 값입니다." });
+        }
+        const updateData = req.body;
+        books[index] = { ...books[index], ...updateData };
+
+        res.status(200).json({ message: "도서 한권 수정하기", book: books[index] })
+    } catch (error) {
+        console.error("도서 1권 수정하기 중 오류")
+        res.status(500).json({ message: "서버오류" })
+    }
+})
+router.delete('/:id', (req, res) => {
+    try {
+        const bookId = Number(req.params.id)
+
+        const index = findeIndexId(bookId)
+        if (index === -1) {
+            return res.status(404).json({ message: "유효하지 않은 id 값입니다." });
+        }
+        books.splice(index, 1)
+
+        res.status(200).json({ message: "도서 한권 수정하기", books })
+    } catch (error) {
+        console.error("도서 1권 수정하기 중 오류")
         res.status(500).json({ message: "서버오류" })
     }
 })
 
+router.patch('/:id/title', (req, res) => {
+    try {
+        const bookId = Number(req.params.id)
+
+        const index = findeIndexId(bookId)
+
+        if (index === -1) {
+            return res.status(404).json({ message: "유효하지 않은 id 값입니다." });
+        }
+        const { title } = req.body;
+        if (typeof title !== 'string' || title.trim() === '') {
+            return res.status(400).json({ message: "title은 비어있지 않은 문자열이어야 합니다." });
+        }
+        books[index] = {
+            ...books[index],
+            title
+        };
+
+        res.status(200).json({ message: "도서 한권 제목수정하기", book: books[index] })
+    } catch (error) {
+        console.error("도서 1권 제목수정하기 중 오류")
+        res.status(500).json({ message: "서버오류" })
+    }
+})
+router.patch('/:id/auther', (req, res) => {
+    try {
+        const bookId = Number(req.params.id)
+
+        const index = findeIndexId(bookId)
+
+        if (index === -1) {
+            return res.status(404).json({ message: "유효하지 않은 id 값입니다." });
+        }
+        const { auther } = req.body;
+        if (typeof auther !== 'string' || auther.trim() === '') {
+            return res.status(400).json({ message: "title은 비어있지 않은 문자열이어야 합니다." });
+        }
+        books[index] = {
+            ...books[index],
+            auther
+        };
+
+        res.status(200).json({ message: "도서 한권 제목수정하기", book: books[index] })
+    } catch (error) {
+        console.error("도서 1권 제목수정하기 중 오류")
+        res.status(500).json({ message: "서버오류" })
+    }
+})
 
 module.exports = router
